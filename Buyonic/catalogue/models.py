75c4b0130f672f2f483dddd3db0b8a2d98201916
Jenil-Savla <you@example.com>
2021-12-18 22:17:52 +0530

    Added Product Model

--- Buyonic/catalogue/models.py
+++ Buyonic/catalogue/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from accounts.models import MyUser
 
-# Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length = 25)
+
+class Product(models.Model):
+    manufacturer = models.ForeignKey(MyUser, on_delete = models.CASCADE)
+    category = models.ForeignKey(Category,on_delete = models.CASCADE)
+    name = models.CharField(max_length = 25)
+    cost = models.IntegerField()
+    description = models.TextField(max_length = 100)
+    stock_status = models.BooleanField(default = True)
+    created_on = models.DateTimeField(auto_now_add = True)
+    photo = models.ImageField(blank = True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['cost']
--- Buyonic/catalogue/views.py
+++ Buyonic/catalogue/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from drf_yasg.openapi import Response
+from rest_framework import serializers
+from rest_framework.serializers import Serializer
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+from rest_framework.generics import GenericAPIView
+from rest_framework import mixins,status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+class ProductList(mixins.ListModelMixin,GenericAPIView):
+
+    serializer_class = ProductSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = [IsAuthenticated,]
+    querset = Product.objects.all()
+
+    def get(self,request):
+        serializer = self.serializer_class()
+        return Response(serializer.data, status=status.HTTP_200_OK)
