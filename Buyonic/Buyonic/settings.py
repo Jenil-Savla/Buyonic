a9a09eeacbe63480982adab48cf6f59e3bde8777
Jenil-Savla <you@example.com>
2021-12-18 16:08:27 +0530

    Updated settings.py file

--- Buyonic/Buyonic/settings.py
+++ Buyonic/Buyonic/settings.py
@@ -68,7 +68,7 @@ REST_FRAMEWORK = { 'DEFAULT_AUTHENTICATION_CLASSES' : [
 ]
 }
 
-AUTH_USER_MODEL = 'Buyonic.accounts.MyUser'
+AUTH_USER_MODEL = 'accounts.MyUser'
 
 ROOT_URLCONF = 'Buyonic.urls'
 
@@ -138,12 +138,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+#PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
