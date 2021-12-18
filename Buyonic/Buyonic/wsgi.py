575f43c3575ad9099a1795e9dfcd243f0bfd03e2
Jenil-Savla <you@example.com>
2021-12-18 15:43:11 +0530

    Updated .wsgi file

--- Buyonic/Buyonic/wsgi.py
+++ Buyonic/Buyonic/wsgi.py
@@ -12,6 +12,6 @@ from . import settings
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Buyonic.Buyonic.settings')
 
 application = get_wsgi_application()
