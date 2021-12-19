29574963e9c6a643228d826c4f09d4f5e019f7ef
Jenil-Savla <you@example.com>
2021-12-19 16:22:55 +0530

    Added Email Verification Feature

--- Buyonic/Buyonic/settings.py
+++ Buyonic/Buyonic/settings.py
@@ -167,7 +167,6 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
