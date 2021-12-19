be1e71159d88c4c84f88e5a1fbaddb7c99d9bfee
Jenil-Savla <you@example.com>
2021-12-19 09:29:10 +0530

    Added updated settings.py

--- Buyonic/Buyonic/settings.py
+++ Buyonic/Buyonic/settings.py
@@ -157,11 +157,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT  =   os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-'''STATICFILES_DIRS = (
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)'''
+)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
