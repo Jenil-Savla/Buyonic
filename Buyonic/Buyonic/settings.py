2bbcdeab8fc777dc26cc700234e40f159167d9b1
Jenil-Savla <you@example.com>
2021-12-19 09:25:56 +0530

    Added updated settings.py

--- Buyonic/Buyonic/settings.py
+++ Buyonic/Buyonic/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 #PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
@@ -156,10 +157,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT  =   os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (
+'''STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
+)'''
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
